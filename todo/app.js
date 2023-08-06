e2dacf583ab9282f5f09e4234dc4c1d440f25704
kopokczo <141357204+kopokczo@users.noreply.github.com>
2023-08-06 23:17:46 +0200

    refactor (#1)
    Co-authored-by: bartekguz <bartoszguzik0@gmail.com>

--- todo/app.js
+++ todo/app.js
@@ -1,13 +1,34 @@
-const btn = document.getElementById("btn");
-let text = document.getElementById("textbox");
-const tekst = document.getElementById("table");
+const addTaskButton = document.getElementById("btn");
+const taskTextInput = document.getElementById("textbox");
+const tasksList = document.getElementById("table");
+const deleteLastTaskButton = document.getElementById('delete-button');
 
+const createLiElementWithDeleteButton = () => {
+  return document.createElement('li');
+}
 
-btn.addEventListener('click', function(){
-const textValue = textbox.value;
-const toDo = document.createElement('li');
-toDo.textContent = textValue;
-tekst.appendChild(toDo);
-});
+const appendChildToList = (list, element) => {
+  list.appendChild(element);
+}
 
+const addTextToElement = (element, text) => {
+  element.textContent = text;
+}
 
+const onRemoveLastElement = () => {
+  const selectedElements = tasksList.querySelectorAll("li");
+  const lastElementIndex = selectedElements.length - 1;
+  const lastElement = selectedElements[lastElementIndex];
+
+  tasksList.removeChild(lastElement);
+} 
+
+const onCreateElementClick = () => {
+  const createdElement = createLiElementWithDeleteButton();
+
+  addTextToElement(createdElement, taskTextInput.value);
+  appendChildToList(tasksList, createdElement);      
+}
+
+addTaskButton.addEventListener('click', onCreateElementClick);
+deleteLastTaskButton.addEventListener('click', onRemoveLastElement);
